8a03875d8bb41ebf6a2dd1affc7ff9cb8c5af0bb
Jonathan Varghese <jonathanvarghese24@gmail.com>
2024-03-11 09:35:24 -0500

    Initial Commit

---
+++ Valorant Agent API/Valorant_Agent_APIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Valorant_Agent_APIApp.swift
+//  Valorant Agent API
+//
+//  Created by Jonathan Varghese on 3/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Valorant_Agent_APIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
